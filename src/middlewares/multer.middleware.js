ecd2f894af0b6a69c975c4992f5ad11cd922dafa
react course <rawalakshay99@gmail.com>
2024-09-15 07:48:53 +0530

    routes and controllers

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -8,6 +8,6 @@ const storage = multer.diskStorage({
       cb(null, file.originalname)
     }
   })
+
   
-  
-  export const upload = multer({ storage }) 
+  export const upload = multer({ storage })  
